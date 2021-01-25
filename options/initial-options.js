2b984edf94912c95788a908c03e849e508f05c67
Lutz Schröeer <latz@elektroelch.de>
2021-01-25 16:39:42 +0100

    User can submit host by pressing Return.

--- options/initial-options.js
+++ options/initial-options.js
@@ -11,27 +11,35 @@ var ncHost, host_msg, btn_checkHost, btn_saveHost, input_host, close_button;
 
 document.onreadystatechange = async () => {
   if (document.readyState === 'complete') {
-    document.getElementById('btn_checkHost').addEventListener('click', async (e) => {
+    document.getElementById('btn_checkHost').addEventListener('click', (e) => {
       e.preventDefault();
-      let host = document.getElementById('host').value;
-      let response = await checkHost(host);
-      if (!response.login) {
-        document.getElementById('host_msg').innerHTML = `${response.status} - ${response.statusText}`;
-      } else {
-        // let the polling run in the background and close the popup
-        browser.runtime.sendMessage({
-          msg: 'poll',
-          host,
-          login: response.login,
-          endpoint: response.poll.endpoint,
-          token: response.poll.token,
-        });
-        // TODO: Supress error that connection has been lost
-        window.close();
-      }
+      saveHost();
+    });
+    document.getElementById('host').addEventListener('keyup', (e) => {
+      e.preventDefault();
+      if (e.keyCode === 13) saveHost();
     });
   }
 };
+// -----------------------------------------------------------------------------------------
+async function saveHost() {
+  let host = document.getElementById('host').value;
+  let response = await checkHost(host);
+  if (!response.login) {
+    document.getElementById('host_msg').innerHTML = `${response.status} - ${response.statusText}`;
+  } else {
+    // let the polling run in the background and close the popup
+    browser.runtime.sendMessage({
+      msg: 'poll',
+      host,
+      login: response.login,
+      endpoint: response.poll.endpoint,
+      token: response.poll.token,
+    });
+    // TODO: Supress error that connection has been lost
+    window.close();
+  }
+}
 // -----------------------------------------------------------------------------------------
 async function checkHost(host) {
   const host_msg = document.getElementById('host_msg');
@@ -45,17 +53,3 @@ async function checkHost(host) {
   host_msg.classList.remove('loader-wave');
   return response;
 }
-// ---------------------------------------------------------------------------------------------------------
-async function saveHost() {
-  const nchost = document.getElementById('ncHost').value;
-
-  let x = await store_data('credentials', { nchost });
-  let result = await load_data('credentials', nchost);
-
-  const btn_saveHost = document.getElementById('btn_saveHost');
-  btn_saveHost.classList.add('checkOK');
-  toggleDiv(document.getElementById('credentials'));
-  btn_saveCredentials.disabled = true;
-  document.getElementById('username').focus();
-}
-// ---------------------------------------------------------------------------------------------------------
