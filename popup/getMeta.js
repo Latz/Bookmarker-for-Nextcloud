4323a5140283f2fa6245e73f077ff6f40e0d0db3
Lutz Schröeer <latz@elektroelch.de>
2021-05-31 13:08:09 +0200

    Added "twitter:description" to autoDesc feature

--- popup/getMeta.js
+++ popup/getMeta.js
@@ -1,6 +1,17 @@
-export default async function getMeta(activeTab, content, metaName) {
+// export default async function getMeta(activeTab, content, metaName) {
+//   const dom = new DOMParser().parseFromString(content, 'text/html');
+//   let metas = dom.querySelectorAll('meta');
+//   const meta = dom.querySelector(`[name=${metaName} i] `)?.content;
+//   return meta;
+// }
+
+export default async function getMeta(activeTab, content, ...metaNames) {
   const dom = new DOMParser().parseFromString(content, 'text/html');
   let metas = dom.querySelectorAll('meta');
-  const meta = dom.querySelector(`[name=${metaName} i] `)?.content;
-  return meta;
+  metaNames.forEach((metaName) => {
+    let meta = dom.querySelector(`[name='${metaName}' i]`)?.content;
+    console.log('meta :>> ', meta);
+    if (meta) return meta;
+  });
+  return ''; // no description found
 }
