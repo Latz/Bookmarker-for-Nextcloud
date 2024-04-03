b67e7f7dd8f4940e475cf31161619d698fe84b1b
Latz <latz@elektroelch.de>
2024-04-03 19:27:30 +0200

    Removed obsolete CSS files

--- src/options/displayJson.js
+++ src/options/displayJson.js
@@ -3,7 +3,7 @@ import { load_data_all } from '../lib/storage';
 
 const urlParams = window.location.search || '';
 const type = urlParams.split('=')[1];
-var data;
+let data;
 if (type === 'options') data = await load_data_all('options');
 if (type === 'cache') {
   const db = await openDB('BookmarkerCache', 2);
