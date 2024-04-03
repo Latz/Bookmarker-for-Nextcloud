3e7ed1946c28a3b734eb99a26feaf362130ed381
Latz <latz@elektroelch.de>
2024-04-03 15:39:46 +0200

    Added buttons to display options and cache.

---
+++ src/options/displayJson.js
@@ -0,0 +1,13 @@
+import { openDB } from 'idb';
+import { load_data_all } from '../lib/storage';
+
+const urlParams = window.location.search || '';
+const type = urlParams.split('=')[1];
+var data;
+if (type === 'options') data = await load_data_all('options');
+if (type === 'cache') {
+  const db = await openDB('BookmarkerCache', 2);
+  data = await db.get('keywords', 'keywords');
+}
+document.getElementById('jsondata').innerHTML =
+  '<pre>' + JSON.stringify(data, null, 4) + '</pre>';
