38e4c325a3b52e90a2433d3526541c70da25915c
Latz <latz@elektroelch.de>
2024-07-01 20:49:20 +0200

    ZenMode seems to work. Still needs testing.

--- src/background/modules/zenMode.js
+++ src/background/modules/zenMode.js
@@ -4,17 +4,24 @@ import apiCall from '../../lib/apiCall';
 import { notifyUser } from './notification';
 
 export async function zenMode() {
-  console.log('zenMode');
   const data = await getData();
-  console.log('data:', data);
 
-  const selectedZenFolders = await load_data('options', 'zenFolders');
+  let folders = '';
+  const selectedZenFolderIDs = await load_data('options', 'zenFolderIDs');
+  if (selectedZenFolderIDs !== undefined) {
+    selectedZenFolderIDs.forEach(
+      (folder) => (folders += `&folders[]=${folder}`),
+    );
+  }
+
+  let tags = '';
   const zenKeywords = await load_data('options', 'input_zenKeywords');
-  console.log('zenKeywords', zenKeywords);
+  if (zenKeywords !== undefined) {
+    zenKeywords.forEach((keyword) => (tags += `&tags[]=${keyword}`));
+  }
+  data.keywords.forEach((keyword) => (tags += `&tags[]=${keyword}`));
 
-  const parameters = `title=${encodeURIComponent(data.title)}&url=${data.url}&description=${data.description}&tags[]=${zenKeywords}&folders[]=${selectedZenFolders}&page=-1`;
-
-  console.log('parameters', parameters);
+  const parameters = `title=${encodeURIComponent(data.title)}&url=${data.url}&description=${data.description}${tags}${folders}&page=-1`;
 
   const endpoint = 'index.php/apps/bookmarks/public/rest/v2/bookmark';
   const method = 'POST';
@@ -23,7 +30,6 @@ export async function zenMode() {
   const response = await apiCall(endpoint, method, parameters);
   chrome.action.setBadgeText({ text: '' });
   notifyUser(response);
-  console.log('🚀 ~ zenMode ~ response:', response);
 }
 
 export function enableZenMode(menuItemId) {
